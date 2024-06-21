39cae692d0f83020a1ea7f3425bb2c310834685f
AceHanded <vili.kononen@gmail.com>
2024-06-21 22:59:47 +0300

    Add more config options, automatic soundtrack detection and API tools

--- config.lua
+++ config.lua
@@ -1,20 +1,41 @@
 -- Configuration file for What's That Song?
 
 local config = {
+    -- Option: NotificationColorR
+    -- Description: Dictates the red color saturation (Rgb) of the notification.
+    -- Type: Integer | Values: [0 ... 255] | Default: 255
+    ["NotificationColorR"] = 255
+    ,
+    -- Option: NotificationColorG
+    -- Description: Dictates the green color saturation (rGb) of the notification.
+    -- Type: Integer | Values: [0 ... 255] | Default: 255
+    ["NotificationColorG"] = 255
+    ,
+    -- Option: NotificationColorB
+    -- Description: Dictates the blue color saturation (rgB) of the notification.
+    -- Type: Integer | Values: [0 ... 255] | Default: 255
+    ["NotificationColorB"] = 255
+    ,
     -- Option: NotificationDuration
-    -- Description: Dictates the duration for which the "Now playing" notification is displayed.
+    -- Description: Dictates the duration for which the notification is displayed.
     --              Duration is measured in seconds.
     -- Type: Integer | Values: [1 ... ] | Default: 10
     ["NotificationDuration"] = 10
     ,
     -- Option: NotificationSpeed
-    -- Description: Dictates the speed at which the "Now playing" notification is displayed.
+    -- Description: Dictates the speed at which the notification is displayed.
     --              Speed is measured as characters per second.
     -- Type: Integer | Values: [1 ... ] | Default: 12
     ["NotificationSpeed"] = 12
     ,
+    -- Option: NotificationBorder
+    -- Description: Dictates the border size of the notification.
+    --              Value of '0' hides the notification entirely.
+    -- Type: Float | Values: [0 ... 1] | Default: 1.0
+    ["NotificationBorder"] = 1.0
+    ,
     -- Option: ConstantDisplay
-    -- Description: Whether to constantly display the "Now playing" notification.
+    -- Description: Whether to constantly display the notification.
     --              true = Constantly display the notification.
     --              false = Do not constantly display the notification.
     -- Type: Boolean | Values: true, false | Default: false
@@ -32,9 +53,22 @@ local config = {
     -- Type: Integer | Values: [-240 ... 30] | Default: 0
     ["OffsetY"] = 0
     ,
-    -- Option: AntibirthMusicEnabled
-    -- Description: Whether you are using the Antibirth soundtrack instead.
-    -- Type: Boolean | Values: true, false | Default: false
-    ["AntibirthMusicEnabled"] = false
+    -- Option: SizeX
+    -- Description: Dictates the size modifier of the notification's X axis.
+    -- Type: Float | Values: [0 ... 1] | Default: 0.5
+    ["SizeX"] = 0.5
+    ,
+    -- Option: SizeY
+    -- Description: Dictates the size modifier of the notification's Y axis.
+    -- Type: Float | Values: [0 ... 1] | Default: 0.5
+    ["SizeY"] = 0.5
+    ,
+    -- Option: SoundtrackChoice
+    -- Description: Dictates the usage of a specific soundtrack.
+    --              "auto" = Automatically choose the appropriate soundtrack.
+    --              "repentance" = Use the official repentance soundtrack.
+    --              "antibirth" = Use the antibirth soundtrack.
+    -- Type: String | Values: "auto", "repentance", "antibirth" | Default: "auto"
+    ["SoundtrackChoice"] = "auto"
 }
 return config
--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local mod = RegisterMod("What's That Song?", 1)
+whatsThatSong = RegisterMod("What's That Song?", 1)
 local musicManager = MusicManager()
 
 -- Read the config table from the configuration file
@@ -9,10 +9,16 @@ local function validateConfig()
 
     -- Iterate through each key-value pair in the config table, and make sure their value is valid
     for key, value in pairs(config) do
-        if key == "ConstantDisplay" or key == "AntibirthMusicEnabled" then
+        if key == "ConstantDisplay" then
             if type(value) ~= "boolean" then
                 local toboolean = {["false"] = false, [0] = false, ["true"] = true, [1] = true}
                 config[key] = toboolean[value] or false
+            end
+        elseif key == "SoundtrackChoice" then
+            if type(value) ~= "string" then
+                config[key] = "auto"
+            elseif string.lower(value) ~= "auto" and string.lower(value) ~= "repentance" and string.lower(value) ~= "antibirth" then
+                config[key] = "auto"
             end
         else
             if key == "OffsetX" then
@@ -21,8 +27,14 @@ local function validateConfig()
                 min, max, default = -240, 30, 0
             elseif key == "NotificationSpeed" then
                 min, max, default = 1, nil, 12
-            else
+            elseif key == "NotificationDuration" then
                 min, max, default = 1, nil, 10
+            elseif key == "NotificationColorR" or key == "NotificationColorG" or key == "NotificationColorB" then
+                min, max, default = 0, 255, 255
+            elseif key == "NotificationBorder" then
+                min, max, default = 0, 1, 1
+            else
+                min, max, default = 0, 1, 0.5
             end
             
             if type(value) ~= "number" then
@@ -37,63 +49,73 @@ local function validateConfig()
 end
 config = validateConfig()
 
+local function useRepentanceMusic()
+    if config["SoundtrackChoice"] == "auto" then
+        return not antibirthmusicplusplus
+    elseif config["SoundtrackChoice"] == "repentance" then
+        return true
+    else
+        return false
+    end
+end
+
 -- Table containing all MusicIDs and their corresponding soundtrack songs
 local musicIDs = {
     [Music.MUSIC_NULL] = "",
-    [Music.MUSIC_BASEMENT] = not config.AntibirthMusicEnabled and "Ridiculon - Diptera Sonata" or "mudeth - Innocence Glitched",
-    [Music.MUSIC_CAVES] = not config.AntibirthMusicEnabled and "Ridiculon - Sodden Hollow" or "mudeth - Subterranean Homesick Malign",
-    [Music.MUSIC_DEPTHS] = not config.AntibirthMusicEnabled and "Ridiculon - Abyss" or "mudeth - Innocence Mangled",
-    [Music.MUSIC_CELLAR] = not config.AntibirthMusicEnabled and "Ridiculon - Periculum" or "mudeth - Outside the Fold",
-    [Music.MUSIC_CATACOMBS] = not config.AntibirthMusicEnabled and "Ridiculon - Capiticus Calvaria" or "mudeth - Marble Forest",
-    [Music.MUSIC_NECROPOLIS] = not config.AntibirthMusicEnabled and "Ridiculon - When Blood Dries" or "mudeth - The Hammer of Pompeii",
-    [Music.MUSIC_WOMB_UTERO] = not config.AntibirthMusicEnabled and "Ridiculon - Viscera" or "mudeth - Dystension",
-    [Music.MUSIC_GAME_OVER] = not config.AntibirthMusicEnabled and "Ridiculon - Acceptance" or "",
-    [Music.MUSIC_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Crusade" or "mudeth - Invictus",
-    [Music.MUSIC_CATHEDRAL] = not config.AntibirthMusicEnabled and "Ridiculon - Everlasting Hymn" or "mudeth - The Thief",
-    [Music.MUSIC_SHEOL] = not config.AntibirthMusicEnabled and "Ridiculon - Duress" or "mudeth - Shadowdance",
-    [Music.MUSIC_DARK_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Devoid" or "mudeth - Morphine",
-    [Music.MUSIC_CHEST] = not config.AntibirthMusicEnabled and "Ridiculon - Sketches of Pain" or "mudeth - Ultimort",
-    [Music.MUSIC_BURNING_BASEMENT] = not config.AntibirthMusicEnabled and "Ridiculon - Fundamentum" or "mudeth - Flashpoint",
-    [Music.MUSIC_FLOODED_CAVES] = not config.AntibirthMusicEnabled and "Ridiculon - Kave Diluvii" or "mudeth - Foreigner in Zeal",
-    [Music.MUSIC_DANK_DEPTHS] = not config.AntibirthMusicEnabled and "Ridiculon - Pulso Profundum" or "mudeth - Mithraeum",
-    [Music.MUSIC_SCARRED_WOMB] = not config.AntibirthMusicEnabled and "Ridiculon - Cicatrix" or "mudeth - Lethe",
-    [Music.MUSIC_BLUE_WOMB] = not config.AntibirthMusicEnabled and "Ridiculon - Nativitate" or "mudeth - An Armistice",
+    [Music.MUSIC_BASEMENT] = useRepentanceMusic() and "Ridiculon - Diptera Sonata" or "mudeth - Innocence Glitched",
+    [Music.MUSIC_CAVES] = useRepentanceMusic() and "Ridiculon - Sodden Hollow" or "mudeth - Subterranean Homesick Malign",
+    [Music.MUSIC_DEPTHS] = useRepentanceMusic() and "Ridiculon - Abyss" or "mudeth - Innocence Mangled",
+    [Music.MUSIC_CELLAR] = useRepentanceMusic() and "Ridiculon - Periculum" or "mudeth - Outside the Fold",
+    [Music.MUSIC_CATACOMBS] = useRepentanceMusic() and "Ridiculon - Capiticus Calvaria" or "mudeth - Marble Forest",
+    [Music.MUSIC_NECROPOLIS] = useRepentanceMusic() and "Ridiculon - When Blood Dries" or "mudeth - The Hammer of Pompeii",
+    [Music.MUSIC_WOMB_UTERO] = useRepentanceMusic() and "Ridiculon - Viscera" or "mudeth - Dystension",
+    [Music.MUSIC_GAME_OVER] = useRepentanceMusic() and "Ridiculon - Acceptance" or "",
+    [Music.MUSIC_BOSS] = useRepentanceMusic() and "Ridiculon - Crusade" or "mudeth - Invictus",
+    [Music.MUSIC_CATHEDRAL] = useRepentanceMusic() and "Ridiculon - Everlasting Hymn" or "mudeth - The Thief",
+    [Music.MUSIC_SHEOL] = useRepentanceMusic() and "Ridiculon - Duress" or "mudeth - Shadowdance",
+    [Music.MUSIC_DARK_ROOM] = useRepentanceMusic() and "Ridiculon - Devoid" or "mudeth - Morphine",
+    [Music.MUSIC_CHEST] = useRepentanceMusic() and "Ridiculon - Sketches of Pain" or "mudeth - Ultimort",
+    [Music.MUSIC_BURNING_BASEMENT] = useRepentanceMusic() and "Ridiculon - Fundamentum" or "mudeth - Flashpoint",
+    [Music.MUSIC_FLOODED_CAVES] = useRepentanceMusic() and "Ridiculon - Kave Diluvii" or "mudeth - Foreigner in Zeal",
+    [Music.MUSIC_DANK_DEPTHS] = useRepentanceMusic() and "Ridiculon - Pulso Profundum" or "mudeth - Mithraeum",
+    [Music.MUSIC_SCARRED_WOMB] = useRepentanceMusic() and "Ridiculon - Cicatrix" or "mudeth - Lethe",
+    [Music.MUSIC_BLUE_WOMB] = useRepentanceMusic() and "Ridiculon - Nativitate" or "mudeth - An Armistice",
     [Music.MUSIC_UTERO] = "Ridiculon - Caesarian",
-    [Music.MUSIC_MOM_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Matricide" or "mudeth - The Turn",
-    [Music.MUSIC_MOMS_HEART_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Ventricide" or "mudeth - Gloria Filio",
-    [Music.MUSIC_ISAAC_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Infanticide" or "mudeth - Misericorde",
-    [Music.MUSIC_SATAN_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Hericide" or "mudeth - Spectrum of Sin",
-    [Music.MUSIC_DARKROOM_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - The Fallen Angel" or "mudeth - Fitnah",
-    [Music.MUSIC_BLUEBABY_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Ascension" or "mudeth - Rapturepunk",
-    [Music.MUSIC_BOSS2] = not config.AntibirthMusicEnabled and "Ridiculon - Cerebrum Dispersio" or "mudeth - Tandava",
-    [Music.MUSIC_HUSH_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Morituros" or "mudeth - Howl",
-    [Music.MUSIC_ULTRAGREED_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Chorus Mortis" or "mudeth - Non Funkible Token",
-    [Music.MUSIC_LIBRARY_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Tome of Knowledge" or "mudeth - Lucidate",
-    [Music.MUSIC_SECRET_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - The Forgotten" or "mudeth - Forgotten Lullaby",
-    [Music.MUSIC_SECRET_ROOM2] = not config.AntibirthMusicEnabled and "Ridiculon - Super Secret Room" or "mudeth - Forgotten Lullaby",
-    [Music.MUSIC_DEVIL_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Anima Vendit" or "mudeth - Blackpath",
-    [Music.MUSIC_ANGEL_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Empty Vessels" or "mudeth - Whitepath",
-    [Music.MUSIC_SHOP_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Murmur of the Harvestman" or "mudeth - Depression Shop",
-    [Music.MUSIC_ARCADE_ROOM] = not config.AntibirthMusicEnabled and "Ridiculon - Genesis 13:37" or "mudeth - Esc",
-    [Music.MUSIC_BOSS_OVER] = not config.AntibirthMusicEnabled and "Ridiculon - The Calm" or "mudeth - Spinning Out of Orbit",
-    [Music.MUSIC_CHALLENGE_FIGHT] = not config.AntibirthMusicEnabled and "Ridiculon - Ambush" or "",
-    [Music.MUSIC_BOSS_RUSH] = not config.AntibirthMusicEnabled and "Ridiculon - Armageddon" or "mudeth - A Baleful Circus",
+    [Music.MUSIC_MOM_BOSS] = useRepentanceMusic() and "Ridiculon - Matricide" or "mudeth - The Turn",
+    [Music.MUSIC_MOMS_HEART_BOSS] = useRepentanceMusic() and "Ridiculon - Ventricide" or "mudeth - Gloria Filio",
+    [Music.MUSIC_ISAAC_BOSS] = useRepentanceMusic() and "Ridiculon - Infanticide" or "mudeth - Misericorde",
+    [Music.MUSIC_SATAN_BOSS] = useRepentanceMusic() and "Ridiculon - Hericide" or "mudeth - Spectrum of Sin",
+    [Music.MUSIC_DARKROOM_BOSS] = useRepentanceMusic() and "Ridiculon - The Fallen Angel" or "mudeth - Fitnah",
+    [Music.MUSIC_BLUEBABY_BOSS] = useRepentanceMusic() and "Ridiculon - Ascension" or "mudeth - Rapturepunk",
+    [Music.MUSIC_BOSS2] = useRepentanceMusic() and "Ridiculon - Cerebrum Dispersio" or "mudeth - Tandava",
+    [Music.MUSIC_HUSH_BOSS] = useRepentanceMusic() and "Ridiculon - Morituros" or "mudeth - Howl",
+    [Music.MUSIC_ULTRAGREED_BOSS] = useRepentanceMusic() and "Ridiculon - Chorus Mortis" or "mudeth - Non Funkible Token",
+    [Music.MUSIC_LIBRARY_ROOM] = useRepentanceMusic() and "Ridiculon - Tome of Knowledge" or "mudeth - Lucidate",
+    [Music.MUSIC_SECRET_ROOM] = useRepentanceMusic() and "Ridiculon - The Forgotten" or "mudeth - Forgotten Lullaby",
+    [Music.MUSIC_SECRET_ROOM2] = useRepentanceMusic() and "Ridiculon - Super Secret Room" or "mudeth - Forgotten Lullaby",
+    [Music.MUSIC_DEVIL_ROOM] = useRepentanceMusic() and "Ridiculon - Anima Vendit" or "mudeth - Blackpath",
+    [Music.MUSIC_ANGEL_ROOM] = useRepentanceMusic() and "Ridiculon - Empty Vessels" or "mudeth - Whitepath",
+    [Music.MUSIC_SHOP_ROOM] = useRepentanceMusic() and "Ridiculon - Murmur of the Harvestman" or "mudeth - Depression Shop",
+    [Music.MUSIC_ARCADE_ROOM] = useRepentanceMusic() and "Ridiculon - Genesis 13:37" or "mudeth - Esc",
+    [Music.MUSIC_BOSS_OVER] = useRepentanceMusic() and "Ridiculon - The Calm" or "mudeth - Spinning Out of Orbit",
+    [Music.MUSIC_CHALLENGE_FIGHT] = useRepentanceMusic() and "Ridiculon - Ambush" or "",
+    [Music.MUSIC_BOSS_RUSH] = useRepentanceMusic() and "Ridiculon - Armageddon" or "mudeth - A Baleful Circus",
     [Music.MUSIC_JINGLE_BOSS_RUSH_OUTRO] = "",
     [Music.MUSIC_BOSS3] = "Ridiculon - Juggernaut",
     [Music.MUSIC_JINGLE_BOSS_OVER3] = "",
-    [Music.MUSIC_MOTHER_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Morticide" or "mudeth - Memento Mori",
+    [Music.MUSIC_MOTHER_BOSS] = useRepentanceMusic() and "Ridiculon - Morticide" or "mudeth - Memento Mori",
     [Music.MUSIC_DOGMA_BOSS] = "Ridiculon - Living in the Light",
     [Music.MUSIC_BEAST_BOSS] = "Ridiculon - Revelations 13-1",
     [Music.MUSIC_JINGLE_MOTHER_OVER] = "",
     [Music.MUSIC_JINGLE_DOGMA_OVER] = "",
     [Music.MUSIC_JINGLE_BEAST_OVER] = "",
-    [Music.MUSIC_PLANETARIUM] = not config.AntibirthMusicEnabled and "Ridiculon - Stargazer" or "mudeth - Journey from a Jar to the Sky",
+    [Music.MUSIC_PLANETARIUM] = useRepentanceMusic() and "Ridiculon - Stargazer" or "mudeth - Journey from a Jar to the Sky",
     [Music.MUSIC_SECRET_ROOM_ALT_ALT] = "Ridiculon - Bygone",
-    [Music.MUSIC_BOSS_OVER_TWISTED] = not config.AntibirthMusicEnabled and "Ridiculon - Dread" or "mudeth - Spinning Intensifies",
-    [Music.MUSIC_CREDITS] = not config.AntibirthMusicEnabled and "Ridiculon - Tribute" or "mudeth - Underscore",
-    [Music.MUSIC_TITLE] = not config.AntibirthMusicEnabled and "Ridiculon - Genesis 22:10" or "mudeth - Descent",
-    [Music.MUSIC_TITLE_AFTERBIRTH] = not config.AntibirthMusicEnabled and "Ridiculon - Regenesis" or "mudeth - Descent",
-    [Music.MUSIC_TITLE_REPENTANCE] = not config.AntibirthMusicEnabled and "Ridiculon - Genesis Retake Light" or "mudeth - Descent",
+    [Music.MUSIC_BOSS_OVER_TWISTED] = useRepentanceMusic() and "Ridiculon - Dread" or "mudeth - Spinning Intensifies",
+    [Music.MUSIC_CREDITS] = useRepentanceMusic() and "Ridiculon - Tribute" or "mudeth - Underscore",
+    [Music.MUSIC_TITLE] = useRepentanceMusic() and "Ridiculon - Genesis 22:10" or "mudeth - Descent",
+    [Music.MUSIC_TITLE_AFTERBIRTH] = useRepentanceMusic() and "Ridiculon - Regenesis" or "mudeth - Descent",
+    [Music.MUSIC_TITLE_REPENTANCE] = useRepentanceMusic() and "Ridiculon - Genesis Retake Light" or "mudeth - Descent",
     [Music.MUSIC_JINGLE_GAME_START_ALT] = "",
     [Music.MUSIC_JINGLE_NIGHTMARE_ALT] = "",
     [Music.MUSIC_MOTHERS_SHADOW_INTRO] = "",
@@ -120,25 +142,26 @@ local musicIDs = {
     [Music.MUSIC_JINGLE_HUSH_OVER] = "",
     [Music.MUSIC_INTRO_VOICEOVER] = "",
     [Music.MUSIC_EPILOGUE_VOICEOVER] = "",
-    [Music.MUSIC_VOID] = not config.AntibirthMusicEnabled and "Ridiculon - Terminus" or "mudeth - Allnoise",
-    [Music.MUSIC_VOID_BOSS] = not config.AntibirthMusicEnabled and "Ridiculon - Delirium" or "mudeth - Terminal Lucidity",
-    [Music.MUSIC_DOWNPOUR] = not config.AntibirthMusicEnabled and "Ridiculon - River of Despair" or "mudeth - Hallowed Ground",
-    [Music.MUSIC_MINES] = not config.AntibirthMusicEnabled and "Ridiculon - Fissure" or "mudeth - Fault Lines",
-    [Music.MUSIC_MAUSOLEUM] = not config.AntibirthMusicEnabled and "Ridiculon - Walls of Denial" or "mudeth - Machine in the Walls",
-    [Music.MUSIC_CORPSE] = not config.AntibirthMusicEnabled and "Ridiculon - Carrion" or "mudeth - Drowning",
+    [Music.MUSIC_VOID] = useRepentanceMusic() and "Ridiculon - Terminus" or "mudeth - Allnoise",
+    [Music.MUSIC_VOID_BOSS] = useRepentanceMusic() and "Ridiculon - Delirium" or "mudeth - Terminal Lucidity",
+    [Music.MUSIC_DOWNPOUR] = useRepentanceMusic() and "Ridiculon - River of Despair" or "mudeth - Hallowed Ground",
+    [Music.MUSIC_MINES] = useRepentanceMusic() and "Ridiculon - Fissure" or "mudeth - Fault Lines",
+    [Music.MUSIC_MAUSOLEUM] = useRepentanceMusic() and "Ridiculon - Walls of Denial" or "mudeth - Machine in the Walls",
+    [Music.MUSIC_CORPSE] = useRepentanceMusic() and "Ridiculon - Carrion" or "mudeth - Drowning",
     [Music.MUSIC_DROSS] = "Ridiculon - Night Soil",
     [Music.MUSIC_ASHPIT] = "Ridiculon - Absentia",
     [Music.MUSIC_GEHENNA] = "Ridiculon - Morning Star",
     -- [Music.MUSIC_MORTIS] = "",
     [Music.MUSIC_ISAACS_HOUSE] = "Ridiculon - Home",
     [Music.MUSIC_FINAL_VOICEOVER] = "",
-    [Music.MUSIC_DOWNPOUR_REVERSE] = not config.AntibirthMusicEnabled and "Ridiculon - River of Despair (Reversed)" or "mudeth - Hallowed Ground (Reversed)",
+    [Music.MUSIC_DOWNPOUR_REVERSE] = useRepentanceMusic() and "Ridiculon - River of Despair (Reversed)" or "mudeth - Hallowed Ground (Reversed)",
     [Music.MUSIC_DROSS_REVERSE] = "Ridiculon - Night Soil (Reversed)",
     [Music.MUSIC_MINESHAFT_AMBIENT] = "",
     [Music.MUSIC_MINESHAFT_ESCAPE] = "",
     [Music.MUSIC_REVERSE_GENESIS] = "Ridiculon - Genesis Retake Light (Reversed)",
     [Music.NUM_MUSIC] = "",
-    [122] = "mudeth - Gloria Filio"
+    [122] = "mudeth - Gloria Filio",
+    [156] = "Shoji Meguro - Specialist"
 }
 local currentSongName = nil
 local previousMusicID = nil
@@ -150,17 +173,17 @@ local maxVisibleChars = 20
 
 Isaac.ConsoleOutput("What's That Song? loaded successfully.\n")
 
-function mod:GetSongName(musicID)
+function whatsThatSong:GetSongName(musicID)
     return musicIDs[musicID] or tostring(musicID)
 end
 
 local function resetScrollParameters(musicID)
-    currentSongName = mod:GetSongName(musicID)
+    currentSongName = whatsThatSong:GetSongName(musicID)
     scrollPos = 0
     displayTimer = displayDuration
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
+whatsThatSong:AddCallback(ModCallbacks.MC_POST_RENDER, function()
     local musicID = musicManager:GetCurrentMusicID()
 
     -- If the music changes, and it is present in the soundtrack, modify the notification
@@ -187,7 +210,7 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
         -- Adjusting the rendered text position according to window size
         local textX = Isaac.GetScreenWidth() - (Isaac.GetScreenWidth() / 2.75)
         local textY = Isaac.GetScreenHeight() - (Isaac.GetScreenHeight() / 1.15)
-        Isaac.RenderScaledText("Now playing: " .. textToRender, textX + config.OffsetX, textY - config.OffsetY, 0.5, 0.5, 1, 1, 1, 1)
+        Isaac.RenderScaledText("Now playing: " .. textToRender, textX + config.OffsetX, textY - config.OffsetY, config.SizeX, config.SizeY, config.NotificationColorR, config.NotificationColorG, config.NotificationColorB, config.NotificationBorder)
         
         scrollPos = scrollPos + scrollSpeed * (1 / 60)
 
@@ -197,3 +220,22 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
         end
     end
 end)
+
+--[[ API ]]--
+
+whatsThatSongAPI = {}
+
+local function addSong(tbl, musicID, name, replace)
+    if replace then
+        tbl[musicID] = name
+    elseif tbl[musicID] == nil then
+        tbl[musicID] = name
+    else
+        return false
+    end
+    return true
+end
+
+function whatsThatSongAPI:addSong(musicID, name, replace)
+    return addSong(musicIDs, musicID, name, replace)
+end
